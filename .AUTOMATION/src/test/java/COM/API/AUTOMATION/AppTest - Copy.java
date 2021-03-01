1e37dee61f5fb79bcbc224f2bba571c1d7c542fa
kranthi <kranthi.kumar836@gmail.com>
2021-03-01 20:12:10 +0530

    adding new file

---
+++ .AUTOMATION/src/test/java/COM/API/AUTOMATION/AppTest - Copy.java
@@ -0,0 +1,41 @@
+package COM.API.AUTOMATION;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+       //this is constructor
+        super( testName );
+        //hello world
+//heelo api;
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
